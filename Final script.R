02b558f59c6ca4e1a916b5a54924613bfabd4fb4
tchinsky <tchinsky@users.noreply.github.com>
2018-10-19 15:08:43 -0400

    commit changes

--- Final script.R
+++ Final script.R
@@ -1,3 +1,6 @@
 ###
-##Authors: Tyler Chinsky, Varada Krishnadas, Tevor Pnix, Andrew Becker
+## Authors: Tyler Chinsky, Varada Krishnadas, Tevor Pnix, Andrew Becker
+## Bioinfomratics Final Project
+## Date: 10/19/18
+## Version 1
 ###
